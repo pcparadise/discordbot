37496bf8e1075c15217c1b5af4a924366bbdfaf3
awesomearvinder <ArvinderDhan@gmail.com>
2021-03-21 16:52:22 -0500

    remove some of the comments in main.py

--- src/main.py
+++ src/main.py
@@ -1,23 +1,23 @@
-import configparser # the module that parses config.ini and makes it readable
-import discord # the actual discord.py wrapper module
-from discord.ext import commands # discord commands module
-import os # allows you to interact with system files
+import configparser 
+import discord 
+from discord.ext import commands 
+import os 
 
-# initializes and parses the config.ini file so its readable
 CONFIG = configparser.ConfigParser() 
 CONFIG.read("./config.ini")
 
-# retrieves value(s) from config.ini and assings them to variables
 TOKEN = CONFIG.get('Discord', 'Token')
 PREFIX = CONFIG.get('Bot', 'Prefix')
-# initializes the bot itself, this can be called whatever you want, but in this case "BOT" will be used
+
 BOT = commands.Bot(command_prefix=PREFIX)
 
-# BOT.remove_command('help') # Uncomment this line to remove the help command so you can create your own, which is there by default
-# loads the cogs folder, which splits the commands and events into mutliple files
-for filename in os.listdir("./src/cogs"): # Lists every file in the cogs directory
-    if filename.endswith(".py"): # if the file ends with .py 
-        filename = filename[:-3] # remove the last 3 characters from the filename
-        BOT.load_extension(f"cogs.{filename}") # loads the extension
+def main():
+    # Initialize all the cogs in the cogs folder.
+    for filename in os.listdir("./src/cogs"): 
+        if filename.endswith(".py"): 
+            filename = filename[:-3] 
+            BOT.load_extension(f"cogs.{filename}") 
 
-BOT.run(TOKEN) # runs the bot with the provided token
+    BOT.run(TOKEN)
+
+main()
