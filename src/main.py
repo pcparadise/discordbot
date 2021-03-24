4513bdd1dec89a80c2a237e201e0e278e27727d3
Ganoosh <swajabbadaja@gmail.com>
2021-03-24 13:42:06 -0700

    remove the example modules from MODULES in main.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ PREFIX = CONFIG.get("Bot", "Prefix")
 BOT = commands.Bot(command_prefix=PREFIX)
 
 # add your module name here once completed
-MODULES = ["example_command_module", "example_event_module"]
+MODULES = []
 
 
 def main():
