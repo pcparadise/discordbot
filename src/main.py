6669748ddf010d0b4f5b9e2b6f643f0eab582a1b
QuaKe8782 <mastermind4560@gmail.com>
2021-03-27 23:30:12 +0000

    Add cog loading print message

--- src/main.py
+++ src/main.py
@@ -14,15 +14,17 @@ PREFIX = CONFIG.get("Bot", "Prefix")
 BOT = commands.Bot(command_prefix=PREFIX)
 
 # add your module name here once completed
-MODULES = []
+MODULES = ["decisions"]
 
 
 def main():
     """
     The first function that runs.
     """
+    print("Bot is starting...")
     for module in MODULES:
         BOT.load_extension(f"cogs.{module}")
+        print(f"cogs.{module} loaded")
     BOT.run(TOKEN)
 
 
