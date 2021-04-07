ba86d1bd9d9e9781aceeb4fbd979c8e91cf35f6a
awesomearvinder <ArvinderDhan@gmail.com>
2021-04-06 19:24:16 -0500

    move get_conf_path into shared_state module

--- src/main.py
+++ src/main.py
@@ -3,39 +3,11 @@ The entry point for the discord bot. You can add modules by adding them
 to the MODULES variable.
 """
 import importlib
-import os
-import pathlib
 import sys
 
-import appdirs
 from discord.ext import commands
 
 from shared_state import SharedState
-
-
-def get_conf_path(filename):
-    """
-    Gets the config from a number of potential
-    spots - either in tree or following the OS
-    specific conventions and rules. On linux
-    this would be the XDG specification for
-    example. Returns None if no path was found.
-    """
-    # get the parent path of the parent path of the current file
-    # this must be just above the "src/" directory
-    program_path = pathlib.Path(__file__).absolute().parent.parent
-    file_path = program_path / filename
-    if os.path.exists(file_path):
-        return str(file_path)
-
-    # get where to store the file via the OS conventions. This is second in
-    # priority from storing it directly with the program.
-    os_conventioned_path = appdirs.user_config_dir("PCParadiseBot") + "/config.ini"
-    if os.path.exists(os_conventioned_path):
-        return os_conventioned_path
-
-    # It wasn't found.
-    return None
 
 
 # add your module name here once completed
@@ -46,8 +18,9 @@ def main():
     """
     The first function that runs.
     """
-    conf_path = get_conf_path("config.ini")
-    if not conf_path:
+    try:
+        config = SharedState("config.ini")
+    except FileNotFoundError:
         print(
             "No config.ini was found\n"
             "valid config paths include the bot folder "
@@ -56,7 +29,6 @@ def main():
             file=sys.stderr,
         )
         return
-    config = SharedState(conf_path)
     bot = commands.Bot(command_prefix=config.prefix)
     bot.remove_command(
         "help"
