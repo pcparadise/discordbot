0ca790b79db175a3caa47592b742da68fa1bbaca
Arvinder <45527519+awsomearvinder@users.noreply.github.com>
2021-04-06 20:53:47 -0500

    Fix #7 (#17)
    * Fix #7
    
    * print error with no conf to stderr instead of stdin
    
    * move get_conf_path into shared_state module

--- src/main.py
+++ src/main.py
@@ -3,15 +3,12 @@ The entry point for the discord bot. You can add modules by adding them
 to the MODULES variable.
 """
 import importlib
+import sys
+
 from discord.ext import commands
+
 from shared_state import SharedState
 
-CONFIG = SharedState("../config.ini")
-
-BOT = commands.Bot(command_prefix=CONFIG.prefix)
-BOT.remove_command(
-    "help"
-)  # removes the default help command, allowing for a replacement.
 
 # add your module name here once completed
 MODULES = ["help"]
@@ -21,18 +18,34 @@ def main():
     """
     The first function that runs.
     """
+    try:
+        config = SharedState("config.ini")
+    except FileNotFoundError:
+        print(
+            "No config.ini was found\n"
+            "valid config paths include the bot folder "
+            "or the OS conventions your OS follows for configs "
+            "(for example .config/ or $XDG_CONFIG_PATH on linux)",
+            file=sys.stderr,
+        )
+        return
+    bot = commands.Bot(command_prefix=config.prefix)
+    bot.remove_command(
+        "help"
+    )  # removes the default help command, allowing for a replacement.
+
     print("Bot is starting...")
-    load_modules()
-    BOT.run(CONFIG.token)
+    load_modules(bot, config)
+    bot.run(config.token)
 
 
-def load_modules():
+def load_modules(bot, config):
     """
-    Imports all modules in a given list.
+    Imports all modules in MODULES
     Passes through bot and config to the setup function.
     """
     for item in MODULES:
-        importlib.import_module("cogs." + item).setup(BOT, CONFIG)
+        importlib.import_module("cogs." + item).setup(bot, config)
 
 
 main()
--- src/shared_state.py
+++ src/shared_state.py
@@ -1,10 +1,15 @@
 """
 Defines a class SharedState which acts as an
 object to be passed around across modules which need
-access to similar data such as the bot prefix
-Takes in a config to initialize itself.
+access to similar data such as the bot prefix.
+Takes in a config file name to initialize itself.
 """
+
 import configparser
+import os
+import pathlib
+
+import appdirs
 
 
 def initialize_config(file_path):
@@ -22,13 +27,41 @@ def initialize_config(file_path):
     return config
 
 
+def get_conf_path(filename):
+    """
+    Gets the config from a number of potential
+    spots - either in tree or following the OS
+    specific conventions and rules. On linux
+    this would be the XDG specification for
+    example. Returns None if no path was found.
+    """
+    # get the parent path of the parent path of the current file
+    # this must be just above the "src/" directory
+    program_path = pathlib.Path(__file__).absolute().parent.parent
+    file_path = program_path / filename
+    if os.path.exists(file_path):
+        return str(file_path)
+
+    # get where to store the file via the OS conventions. This is second in
+    # priority from storing it directly with the program.
+    os_conventioned_path = appdirs.user_config_dir("PCParadiseBot") + "/config.ini"
+    if os.path.exists(os_conventioned_path):
+        return os_conventioned_path
+
+    # It wasn't found.
+    return None
+
+
 class SharedState:  # pylint: disable=too-few-public-methods
     """
     Creates a SharedState object
     SharedState contains information in variables that can be called from anywhere (cog/file).
     """
 
-    def __init__(self, file_path):
+    def __init__(self, filename):
+        file_path = get_conf_path(filename)
+        if not file_path:
+            raise FileNotFoundError
         config = initialize_config(file_path)
         self.initialize_shared_state_variables(config)
 
