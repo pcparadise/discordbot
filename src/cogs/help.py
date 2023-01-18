31130ac3733cb8dea9600abc838f06d2acfd165e
ganoodles <swajabbadaja@gmail.com>
2023-01-17 16:21:22 -0800

    More formatting for help.py

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -89,7 +89,7 @@ class CustomHelp(commands.HelpCommand):
                 continue
             out.add_field(
                 name=command_usage(cmd_or_group),
-                value=(cmd_or_group.help if cmd_or_group.help else "No help given"),
+                value="```" + (cmd_or_group.help if cmd_or_group.help else "No help given") + "```",
                 inline=False,
             )
 
