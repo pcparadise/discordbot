c67ad9c8b216e386c6d400a1589a9212472fc5e9
Arvinder <ArvinderDhan@gmail.com>
2023-04-23 19:49:41 -0500

    feat: nix discordbot package
    It's kind of jank right now. It just runs poetry install and then runs
    it. It might be worth using poetry2nix again in the future but this
    should be fine for now atleast.

--- flake.nix
+++ flake.nix
@@ -14,11 +14,31 @@
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+
+      discordbot = pkgs.stdenv.mkDerivation {
+        name = "discordbot";
+        buildInputs = [pkgs.poetry];
+        src = ./.;
+        buildPhase = ''
+          mkdir -p $out/bin
+          mkdir -p $out/lib
+          cp -R $src/* $out/lib
+          echo "#!${pkgs.bash}/bin/bash
+          ${pkgs.poetry}/bin/poetry -C $out/lib install
+          ${pkgs.poetry}/bin/poetry -C $out/lib run run
+          " > $out/bin/discordbot
+          chmod 555 $out/bin/discordbot
+        '';
+      };
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [pkgs.gcc pkgs.pkg-config];
         buildInputs = [pkgs.poetry];
       };
+
+      # TODO: might be better to use poetry2nix here
+      packages.discordbot = discordbot;
+      defaultPackage = self.packages.${system}.discordbot;
       formatter = alejandra.defaultPackage.${system};
     });
 }
